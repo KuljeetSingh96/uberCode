c91584fb35097bd279f769b690576e7c6ee94231
Kuljeet Singh <singhkuljeet961988@gmail.com>
2019-01-23 20:11:14 +0530

    no message

--- app/src/test/java/com/ubercode/ui/flickerlist/FlickerListSearchPresenterTest.java
+++ app/src/test/java/com/ubercode/ui/flickerlist/FlickerListSearchPresenterTest.java
@@ -9,14 +9,13 @@ import com.ubercode.network.model.FlickerListResponse;
 import com.ubercode.network.repository.Repository;
 import com.ubercode.utils.schedulers.SchedulerProvider;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
-import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.Scheduler;
@@ -56,6 +55,7 @@ public class FlickerListSearchPresenterTest {
     private FlickerListSearchPresenter getFlickerListPresenter()
     {
         viewModel = spy(new FlickerListSearchViewModel());
+        viewModel.pageCount=spy((new MutableLiveData<>()));
         viewModel.errorMessageVisibility=spy(new MutableLiveData<>());
         viewModel.errorMessage =spy(new MutableLiveData<>());
         viewModel.loadingVisibility=spy(new MutableLiveData<>());
@@ -68,99 +68,103 @@ public class FlickerListSearchPresenterTest {
     @Test
     public void testGetFlickerListWithEmptyResponse() throws Exception {
         FlickerListSearchPresenter flickerListSearchPresenter =getFlickerListPresenter();
-//        when(repository.getFlickerImages(METHOD_INPUT, FLICKER_API_KEY, CALLBACK_FORMAT,
-//                NO_JSON_CALLBACK_FLAG, "", PER_PAGE, 1)).
-//                thenReturn(Observable.just(Collections.EMPTY_LIST));
-        flickerListSearchPresenter.getFlickerImages(SEARCH_QUERY);
-        testScheduler.triggerActions();
-        verify(viewModel.errorMessage,times(1)).setValue("No list available.");
-        Mockito.verifyZeroInteractions(viewModel.repoListData);
-        verify(viewModel.errorMessageVisibility,times(2)).setValue(anyInt());
-        verify(viewModel.repoListVisibility,times(2)).setValue(anyInt());
-        verify(viewModel.loadingVisibility,times(2)).setValue(anyInt());
+        FlickerListResponse flickerListResponse = new FlickerListResponse();
+        FlickerListResponse.Photos photos = new FlickerListResponse.Photos();
+        flickerListResponse.setPhotos(photos);
+        try {
+            when(viewModel.pageCount.getValue()).thenReturn(0);
+            when(repository.getFlickerImages(METHOD_INPUT, FLICKER_API_KEY, CALLBACK_FORMAT,
+                    NO_JSON_CALLBACK_FLAG, SEARCH_QUERY, PER_PAGE, 1)).
+                    thenReturn(Observable.just(flickerListResponse));
+            flickerListSearchPresenter.getFlickerImages(SEARCH_QUERY);
+            testScheduler.triggerActions();
+            verify(viewModel.errorMessage,times(1)).setValue("No list available.");
+            Mockito.verifyZeroInteractions(viewModel.repoListData);
+            verify(viewModel.errorMessageVisibility,times(2)).setValue(anyInt());
+            verify(viewModel.repoListVisibility,times(2)).setValue(anyInt());
+            verify(viewModel.loadingVisibility,times(2)).setValue(anyInt());
+        }catch (Exception ex)
+        {
+            Assert.fail("Test failed  : "+ex.getMessage());
+        }
+
     }
+
     @Test
     public void testGetFlickerListWithValidResponse() throws Exception {
-        List<FlickerListResponse> flickerListResponses = getValidList();
+        FlickerListResponse flickerListResponses = getValidList();
 
         FlickerListSearchPresenter flickerListSearchPresenter =getFlickerListPresenter();
-//        when(repository.getFlickerImages(METHOD_INPUT, FLICKER_API_KEY,
-//                CALLBACK_FORMAT, NO_JSON_CALLBACK_FLAG, "kittens",
-//                PER_PAGE, 1)).thenReturn(Observable.just(flickerListResponses));
-
-        flickerListSearchPresenter.getFlickerImages(SEARCH_QUERY);
-        testScheduler.triggerActions();
-        verify(viewModel.errorMessage,never()).setValue("No list available.");
+        when(viewModel.pageCount.getValue()).thenReturn(0);
+        when(repository.getFlickerImages(METHOD_INPUT, FLICKER_API_KEY,
+                CALLBACK_FORMAT, NO_JSON_CALLBACK_FLAG, SEARCH_QUERY,
+                PER_PAGE, 1)).thenReturn(Observable.just(flickerListResponses));
+        try {
+            flickerListSearchPresenter.getFlickerImages(SEARCH_QUERY);
+            testScheduler.triggerActions();
+            verify(viewModel.errorMessage,never()).setValue("No list available.");
 //        verify(viewModel.repoListData,times(1)).postValue(flickerListResponses);
-        verify(viewModel.errorMessageVisibility,times(2)).setValue(anyInt());
-        verify(viewModel.repoListVisibility,times(2)).setValue(anyInt());
-        verify(viewModel.loadingVisibility,times(2)).setValue(anyInt());
+            verify(viewModel.errorMessageVisibility,times(2)).setValue(anyInt());
+            verify(viewModel.repoListVisibility,times(2)).setValue(anyInt());
+            verify(viewModel.loadingVisibility,times(2)).setValue(anyInt());
+        }catch (Exception ex)
+        {
+            Assert.fail("Test failed  : "+ex.getMessage());
+        }
+
+
     }
 
     @Test
     public void testGetFlickerListWithNetworkErrorResponse() throws Exception {
 
         FlickerListSearchPresenter flickerListSearchPresenter =getFlickerListPresenter();
+        when(viewModel.pageCount.getValue()).thenReturn(0);
         when(repository.getFlickerImages(METHOD_INPUT, FLICKER_API_KEY,
                 CALLBACK_FORMAT, NO_JSON_CALLBACK_FLAG, SEARCH_QUERY,
                 PER_PAGE, 1)).thenReturn(Observable.error(new Throwable()));
+        try {
+            flickerListSearchPresenter.getFlickerImages(SEARCH_QUERY);
+            testScheduler.triggerActions();
+            Mockito.verifyZeroInteractions(viewModel.repoListData);
+            verify(viewModel.errorMessage,times(1)).setValue("Network error. Please try again later.");
+            verify(viewModel.errorMessageVisibility,times(2)).setValue(anyInt());
+            verify(viewModel.repoListVisibility,times(2)).setValue(anyInt());
+            verify(viewModel.loadingVisibility,times(2)).setValue(anyInt());
+        }catch (Exception ex)
+        {
+            Assert.fail("Test failed  : "+ex.getMessage());
+        }
 
-        flickerListSearchPresenter.getFlickerImages(SEARCH_QUERY);
-        testScheduler.triggerActions();
-        Mockito.verifyZeroInteractions(viewModel.repoListData);
-        verify(viewModel.errorMessage,times(1)).setValue("Network error. Please try again later.");
-        verify(viewModel.errorMessageVisibility,times(2)).setValue(anyInt());
-        verify(viewModel.repoListVisibility,times(2)).setValue(anyInt());
-        verify(viewModel.loadingVisibility,times(2)).setValue(anyInt());
     }
 
 
-    private List<FlickerListResponse> getValidList()
+    private FlickerListResponse getValidList()
     {
-        String RESPONSE_STRING ="[{\n" +
-                "\t\"username\": \"Microsoft\",\n" +
-                "\t\"name\": \"Microsoft\",\n" +
-                "\t\"url\": \"https://github.com/Microsoft\",\n" +
-                "\t\"avatar\": \"https://avatars2.githubusercontent.com/u/6154722?s=96&v=4\",\n" +
-                "\t\"repo\": {\n" +
-                "\t\t\"name\": \"malmo\",\n" +
-                "\t\t\"description\": \"Project Malmo is a platform for Artificial Intelligence experimentation and research built on top of Minecraft. We aim to inspire a new generation of research into challenging new problems presented by this unique environment. --- For installation instructions, scroll down to *Getting Started* below, or visit the project page for more information:\",\n" +
-                "\t\t\"url\": \"https://github.com/Microsoft/malmo\"\n" +
+        String RESPONSE_STRING ="{\n" +
+                "\t\"photos\": {\n" +
+                "\t\t\"page\": 1,\n" +
+                "\t\t\"pages\": 7798,\n" +
+                "\t\t\"perpage\": 20,\n" +
+                "\t\t\"total\": \"155949\",\n" +
+                "\t\t\"photo\": [{\n" +
+                "\t\t\t\"id\": \"45892860585\",\n" +
+                "\t\t\t\"secret\": \"f9a7c8fdf1\",\n" +
+                "\t\t\t\"server\": \"7865\",\n" +
+                "\t\t\t\"farm\": 8,\n" +
+                "\t\t\t\"title\": \"CAT 03 KH0105 01\"\n" +
+                "\t\t}, {\n" +
+                "\t\t\t\"id\": \"32932164258\",\n" +
+                "\t\t\t\"secret\": \"4c0e55f13c\",\n" +
+                "\t\t\t\"server\": \"7839\",\n" +
+                "\t\t\t\"farm\": 8,\n" +
+                "\t\t\t\"title\": \"Cat\"\n" +
+                "\t\t}]\n" +
                 "\t}\n" +
-                "}, {\n" +
-                "\t\"username\": \"google\",\n" +
-                "\t\"name\": \"Google\",\n" +
-                "\t\"url\": \"https://github.com/google\",\n" +
-                "\t\"avatar\": \"https://avatars0.githubusercontent.com/u/1342004?s=96&v=4\",\n" +
-                "\t\"repo\": {\n" +
-                "\t\t\"name\": \"guava\",\n" +
-                "\t\t\"description\": \"Google core libraries for Java\",\n" +
-                "\t\t\"url\": \"https://github.com/google/guava\"\n" +
-                "\t}\n" +
-                "}, {\n" +
-                "\t\"username\": \"flutter\",\n" +
-                "\t\"name\": \"Flutter\",\n" +
-                "\t\"url\": \"https://github.com/flutter\",\n" +
-                "\t\"avatar\": \"https://avatars1.githubusercontent.com/u/14101776?s=96&v=4\",\n" +
-                "\t\"repo\": {\n" +
-                "\t\t\"name\": \"flutter-intellij\",\n" +
-                "\t\t\"description\": \"Flutter makes it easy and fast to build beautiful mobile apps.\",\n" +
-                "\t\t\"url\": \"https://github.com/flutter/flutter-intellij\"\n" +
-                "\t}\n" +
-                "}, {\n" +
-                "\t\"username\": \"facebook\",\n" +
-                "\t\"name\": \"Facebook\",\n" +
-                "\t\"url\": \"https://github.com/facebook\",\n" +
-                "\t\"avatar\": \"https://avatars2.githubusercontent.com/u/69631?s=96&v=4\",\n" +
-                "\t\"repo\": {\n" +
-                "\t\t\"name\": \"fresco\",\n" +
-                "\t\t\"description\": \"An Android library for managing images and the memory they use.\",\n" +
-                "\t\t\"url\": \"https://github.com/facebook/fresco\"\n" +
-                "\t}\n" +
-                "}]";
+                "}";
         Gson gson = new Gson();
-        List<FlickerListResponse> apiResponse = gson.fromJson(RESPONSE_STRING,
-                new TypeToken<List<FlickerListResponse>>() {
+        FlickerListResponse apiResponse = gson.fromJson(RESPONSE_STRING,
+                new TypeToken<FlickerListResponse>() {
                 }.getType());
         return apiResponse;
     }
